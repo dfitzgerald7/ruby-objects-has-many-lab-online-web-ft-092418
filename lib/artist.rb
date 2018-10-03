4fc809e1209b3c2e21c6f48b5ea30a5cdd3b3c75
Doug Fitzgerald <dfitzge1@conncoll.edu>
2018-10-03 17:58:35 +0000

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,24 @@
+require "pry"
+class Artist 
+  attr_accessor :name, :songs 
+  
+  def initialize(name)
+    @name = name
+    @songs = []
+   
+  end
+  
+  def add_song(song)
+    #binding.pry
+    song.artist = self 
+    self.songs << song unless self.songs.include?(song)
+  end 
+  
+  def add_song_by_name(song_name)
+    Song.new(song_name).tap {|song| add_song(song)}
+  end 
+  
+  def self.song_count 
+    Song.all.length
+  end 
+end 
---
+++ lib/author.rb
@@ -0,0 +1,23 @@
+require "pry"
+class Author 
+  attr_accessor :name, :posts  
+  
+  def initialize(name) 
+    @name = name
+    @posts = []
+  end 
+  
+  def add_post(post)
+    #binding.pry
+    post.author = self
+    self.posts << post unless self.posts.include?(post)
+  end 
+  
+  def add_post_by_title(post)
+    Post.new(post).tap{|post| add_post(post)}
+  end 
+  
+  def self.post_count
+    Post.all.length
+  end 
+end 
