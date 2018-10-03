1e74ad12a38fa0c9d17bd7a5debc1d05b7f240e3
Doug Fitzgerald <dfitzge1@conncoll.edu>
2018-10-03 17:41:14 +0000

    automatically backed up by learn

---
+++ lib/artist.rb
@@ -0,0 +1,19 @@
+require "pry"
+class Artist 
+  attr_accessor :name, :songs 
+  
+  def initialize(name)
+    @name = name
+    @songs = []
+  end
+  
+  def add_song(song)
+    binding.pry
+    song.artist = self 
+    self.songs << song unless self.songs.include?(song)
+  end 
+  
+  def self.song_count 
+    self.songs.count
+  end 
+end 
---
+++ lib/author.rb
@@ -0,0 +1,12 @@
+class Author 
+  attr_accessor :name, :posts  
+  
+  def initialize(name) 
+    @name = name
+    @posts = []
+  end 
+  
+  def add_post(post)
+    post.author = self
+  end 
+end 
